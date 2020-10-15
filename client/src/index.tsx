7ee724837cd0d6d89df6491e3389fcb3889aca7b
Márk Domahidi <mark.domahidi@tappointment.com>
2020-10-15 09:56:30 +0200

    Fixed wrong import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
-import store from './store'
 import theme from './app/theme'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import { ThemeProvider } from '@material-ui/core'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
