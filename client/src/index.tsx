ce2ab26f7d3410edb969391459aa35febd733cfb
Domahidi Márk <mdomahidi@gmail.com>
2020-10-19 17:38:27 +0200

    Fixed error caused by merge commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
+import store from './store'
 import theme from './app/theme'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { ThemeProvider } from '@material-ui/core'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
