4d65fc82d32a1b1041989b1d84edf12e3fe63a8e
Domahidi Márk <mdomahidi@gmail.com>
2020-10-19 13:51:33 +0200

    DRSTP-3 Connected redux store to application

---
+++ client/src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
+
+const store = configureStore({
+  reducer: {},
+  middleware: getDefaultMiddleware => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV === 'development'
+})
+
+type AppDispatch = typeof store.dispatch
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+
+export type RootState = ReturnType<typeof store.getState>
+export default store
