8f5b7dd6c0bf4e599a2546fca9d425dbe52ce01b
Domahidi Márk <mdomahidi@gmail.com>
2020-10-19 17:07:52 +0200

    DRSTP-3 Fixed protected routing

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, RouteProps, Redirect } from 'react-router-dom'
+import { Route, RouteProps, RouteComponentProps, Redirect } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { userTokenSelector } from '../store/user'
 
@@ -11,20 +11,17 @@ type Props = {
 
 const ProtectedRoute: React.FC<Props & RouteProps> = ({ Component, auth, noAuth, ...rest }) => {
   const userToken = useSelector(userTokenSelector)
-  const redirect = noAuth ? '/' : '/login'
 
-  return (
-    <Route
-      {...rest}
-      render={routeProps =>
-        userToken && !noAuth ? (
-          <Component {...routeProps} />
-        ) : (
-          <Redirect to={{ pathname: redirect, state: { from: routeProps.location } }} />
-        )
-      }
-    />
-  )
+  const renderComponent = (routeProps: RouteComponentProps) => {
+    const state = { from: routeProps.location }
+
+    if (!userToken && !noAuth) return <Redirect to={{ pathname: '/login', state }} />
+    if (userToken && noAuth) return <Redirect to={{ pathname: '/', state }} />
+
+    return <Component {...routeProps} />
+  }
+
+  return <Route {...rest} render={routeProps => renderComponent(routeProps)} />
 }
 
 export default ProtectedRoute
