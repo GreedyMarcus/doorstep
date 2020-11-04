1a7ca0474f89d3ae2da9643c125a90a87655f0ac
Domahidi Márk <mdomahidi@gmail.com>
2020-11-04 11:53:29 +0100

    DRSTP-30 Extracted routes to separate file

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import ForgotPassword from '../pages/ForgotPassword'
@@ -8,38 +8,25 @@ import ConsentForms from '../pages/ConsentForms'
 import NavigationBar from '../components/NavigationBar'
 import ProtectedRoute from '../components/ProtectedRoute'
 import ActionTracker from '../components/ActionTracker'
+import { routes } from './routes'
 import { CssBaseline } from '@material-ui/core'
 import { Switch } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 import { UserRole } from '../data/enums/UserRole'
-import { useAppDispatch } from '../store'
-import { userTokenSelector, loadCurrentUser } from '../store/user'
 
-const App = () => {
-  const dispatch = useAppDispatch()
-  const userToken = useSelector(userTokenSelector)
-
-  useEffect(() => {
-    if (userToken) {
-      dispatch(loadCurrentUser(userToken))
-    }
-  }, [userToken])
-
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <NavigationBar />
-      <Switch>
-        <ProtectedRoute exact path="/login" noAuth Component={Login} />
-        <ProtectedRoute exact path="/register" noAuth Component={Register} />
-        <ProtectedRoute exact path="/forgot-password" noAuth Component={ForgotPassword} />
-        <ProtectedRoute exact path="/reset-password/:token" noAuth Component={ResetPassword} />
-        <ProtectedRoute exact path="/companies" auth={[UserRole.ADMIN]} Component={Companies} />
-        <ProtectedRoute exact path="/consent-forms" auth={[UserRole.ADMIN]} Component={ConsentForms} />
-      </Switch>
-      <ActionTracker />
-    </React.Fragment>
-  )
-}
+const App = () => (
+  <React.Fragment>
+    <CssBaseline />
+    <NavigationBar />
+    <Switch>
+      <ProtectedRoute exact path={routes.LOGIN} noAuth Component={Login} />
+      <ProtectedRoute exact path={routes.REGISTER} noAuth Component={Register} />
+      <ProtectedRoute exact path={routes.FORGOT_PASSWORD} noAuth Component={ForgotPassword} />
+      <ProtectedRoute exact path={routes.RESET_PASSWORD} noAuth Component={ResetPassword} />
+      <ProtectedRoute exact path={routes.COMPANIES} auth={[UserRole.ADMIN]} Component={Companies} />
+      <ProtectedRoute exact path={routes.CONSENT_FORMS} auth={[UserRole.ADMIN]} Component={ConsentForms} />
+    </Switch>
+    <ActionTracker />
+  </React.Fragment>
+)
 
 export default App
---
+++ client/src/app/routes.ts
@@ -0,0 +1,12 @@
+export const routes = {
+  LOGIN: '/login',
+  REGISTER: '/register',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: '/reset-password/:token',
+  COMPANIES: '/companies',
+  CONSENT_FORMS: '/consent-forms',
+  VISITS: '/visits',
+  HOSTS: '/hosts',
+  PLANNED_VISITS: '/planned-visits',
+  INVITATIONS: '/invitations'
+}
